35778c5f30feda8206bbeb87530cebc2b8eb8bca
KAYKY COELHO DOS SANTOS <kayky.csantos@senacsp.edu.br>
2025-08-13 17:29:49 -0300

    parciais

--- app.js
+++ app.js
@@ -12,12 +12,19 @@ app.listen(3001);
 
 //acesando a rota
 app.get('/', (req, res) => {
-      res.render('index');
+  //pasando parametros para o body
+  const blogs = [
+       {titulo: 'chamander', conteudo: 'a ponta de seu rabo está constantemente em chamas e se for apagada pode resultar em sua morte.'},
+      {titulo: 'squirtle', conteudo: 'seu casco reduz a resistencia contra a água fazendo com que ele nade mais rapido'},
+      {titulo: 'bulbasur', conteudo: 'evoluir o bulbo começa a desabrochar até se tornar uma grade flor na evolução final'}, 
+  ];
+      res.render('index', { titulo: 'home', blogs});
 });
 
 //nova rota
 app.get('/sobre', (req, res) =>{
-  res.render('sobre');
+
+  res.render('sobre', { titulo: 'sobre'});
 });
 
 //redirecinamento de pagina
@@ -25,7 +32,14 @@ app.get('/sobrenos', (req, res) =>{
   res.redirect('./sobre');
 });
 
+
+//rota da criação do conteudo
+app.get('/blog/criar', (req, res) => {
+  res.render('criar',{ titulo: 'criar novo blog'});
+});
+
+
 //erro 404
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404',{ titulo: '404'});
 });
